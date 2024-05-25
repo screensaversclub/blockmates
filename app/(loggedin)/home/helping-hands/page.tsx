e2f7b56e546cfe28e64d306846cdcd3f86ffe6f0
Siah <siah@goodwork.sg>
2024-05-25 16:00:27 +0800

    feat: redirect user to home if already onboarded

---
+++ app/(loggedin)/home/helping-hands/page.tsx
@@ -0,0 +1,9 @@
+export default async function HelpingHands() {
+  return (
+    <div>
+      <p>Find help, or offer help to someone!</p>
+      <button type="button">Find Help</button>
+      <button type="button">Offer Help</button>
+    </div>
+  );
+}
