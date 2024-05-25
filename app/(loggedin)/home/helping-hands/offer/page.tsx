73e7d699b302ebd73d26eef27bbda4114ad9bc92
Siah <siah@goodwork.sg>
2024-05-25 19:54:07 +0800

    feat: visuals

--- app/(loggedin)/home/helping-hands/offer/page.tsx
+++ app/(loggedin)/home/helping-hands/offer/page.tsx
@@ -19,31 +19,34 @@ export default async function OfferHelp() {
   }
 
   return (
-    <div className='flex min-h-screen w-full flex-col items-center justify-start gap-4 py-4'>
+    <div className='flex flex-col items-center justify-start w-full min-h-screen py-4 gap-4'>
       <h1 className='mt-8 text-xl'>Help make someone's day!</h1>
       <p>See some requests for help by your neighbours.</p>
 
       <div id='list_of_requests' className='mt-8 grid grid-cols-1 gap-4'>
         {helpRequests.data.map((helpReq) => (
-          <div key={helpReq.id} className='flex items-start gap-4'>
-            <div className='w-16 flex-shrink-0'>
+          <div
+            key={helpReq.id}
+            className='flex items-center px-2 py-4 bg-white rounded shadow gap-4'
+          >
+            <div className='flex-shrink-0 w-16'>
               <UserAvatar userId={helpReq.created_by} />
             </div>
             <div className='mb-2'>
               <div className=''>
-                <h3 className='font-medium'>{helpReq.title}</h3>
+                <h3 className='text-lg font-medium'>{helpReq.title}</h3>
                 <div>
                   {helpReq.themes.map((theme) => (
                     <span
                       key={theme}
-                      className='inline-block rounded border border-green-600 px-2 py-1 text-xs uppercase text-green-600'
+                      className='inline-block px-2 py-1 text-xs text-green-600 uppercase border border-green-600 rounded'
                     >
                       {theme}
                     </span>
                   ))}
                 </div>
               </div>
-              <p className='clamp-3-lines'>{helpReq.body}</p>
+              <p className='text-gray-600 clamp-3-lines'>{helpReq.body}</p>
 
               <small>
                 {format(new Date(helpReq.created_at), "yyyy-MMM-dd HH:mm a")}
@@ -53,7 +56,7 @@ export default async function OfferHelp() {
         ))}
       </div>
 
-      <div className='sticky bottom-0 w-full bg-white p-4'>
+      <div className='sticky bottom-0 w-full p-4 bg-white'>
         <Link href='/home/helping-hands/find' className='w-full'>
           <button className='w-full'>Ask for help</button>
         </Link>
