08b11e9447e1d81395646b7ce9d8809df7be7724
Siah <siah@goodwork.sg>
2024-05-25 16:00:37 +0800

    feat: redirect for onboarded user

--- app/(loggedin)/welcome/page.tsx
+++ app/(loggedin)/welcome/page.tsx
@@ -1,12 +1,31 @@
 import OnboardingForm from "@/components/OnboardingForm";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const WelcomePage: React.FC = async () => {
-	return (
-		<div>
-			<OnboardingForm />
-		</div>
-	);
+  const sb = createClient();
+
+  const user = await sb.auth.getUser();
+
+  if (user.data.user === null) {
+    redirect("/");
+  }
+
+  const metaRow = await sb
+    .from("user_metadata")
+    .select()
+    .eq("user_id", user.data.user.id);
+
+  if (metaRow.data === null || metaRow.data.length > 0) {
+    redirect("/home");
+  }
+
+  return (
+    <div>
+      <OnboardingForm />
+    </div>
+  );
 };
 
 export default WelcomePage;
